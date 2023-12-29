aea402aa32c72918325bf62b16a46bcad8ac380f
Peter Jin <>
2023-12-28 19:22:00 -0500

    Ergonomic api.

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,11 @@ use crate::algo::str::{SafeStr};
 use crate::env::*;
 use crate::parsing::*;
 
+use std::fs::{File};
+use std::io::{Read, BufReader};
+use std::path::{Path};
+use std::str::{from_utf8};
+
 pub mod algo;
 pub mod env;
 pub mod parsing;
@@ -30,4 +35,13 @@ pub fn mangle_js<S: AsRef<str>>(src: &str, globls: &[S]) -> Result<SafeStr, (Cha
   }
   env.mangle_idents();
   Ok(env.render_min().into())
+}
+
+pub fn mangle_js_file<P: AsRef<Path>, S: AsRef<str>>(src_path: P, globls: &[S]) -> Result<SafeStr, (CharSpan, ParseErr)> {
+  let src_file = File::open(src_path).unwrap();
+  let mut reader = BufReader::new(src_file);
+  let mut buf = Vec::new();
+  reader.read_to_end(&mut buf).unwrap();
+  let src = from_utf8(&buf).unwrap();
+  mangle_js(src, globls)
 }
--- tools/mangle_js.rs
+++ tools/mangle_js.rs
@@ -1,9 +1,6 @@
 extern crate jest;
 
 use std::env;
-use std::fs::{File};
-use std::io::{Read, BufReader};
-use std::str::{from_utf8};
 
 fn main() {
   let args: Vec<_> = env::args().collect();
@@ -11,16 +8,12 @@ fn main() {
     println!("usage: jest <input.js> [<global> ...]");
     return;
   }
-  let file = File::open(&args[1]).unwrap();
-  let mut reader = BufReader::new(file);
-  let mut buf = Vec::new();
-  reader.read_to_end(&mut buf).unwrap();
-  let src = from_utf8(&buf).unwrap();
+  let src_path = &args[1];
   let globls = &args[2 .. ];
-  let res = jest::mangle_js(src, globls);
+  let res = jest::mangle_js_file(src_path, globls);
   match res {
     Err(e) => {
-      println!("DEBUG:  mangle_js: err={:?}", e);
+      eprintln!("DEBUG:  mangle_js: err={:?}", e);
     }
     Ok(s) => {
       print!("{}", s.as_raw_str());
